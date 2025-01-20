a8e7796b6e6828251771594e0e5f85bbb4426cf5
shaniBerechya <berechys@post.bgu.ac.il>
2025-01-20 17:18:08 +0000

    string eqvels

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -6,24 +6,26 @@ public class StompServer {
 
     public static void main(String[] args) {
         //Sapty Chack for args
+        System.out.println("args: "+ args.toString() );
         if(args.length != 2){
             System.out.println("args dosent contein port and server type");
         }
+        /*
         else if (args[1] != "reactor" && args[1] != "tpc") {
             System.out.println("server type should be : <reactor / tpc>");
-        }
+        }*/
         else{
             int port = Integer.valueOf(args[0]); //geting the port from args
             //case 1: tpc
-            if (args[1] == "tpc"){
+            if (args[1].equals("tpc")){
                 Server.threadPerClient(
                     port,
                     () -> new StompMessagingProtocolImpl(), //protocol factory,
                     () -> new StompMessageEncoderDecoder())//message encoder decoder factory)
                     .serve(); 
             }
-            //case 2: tpc
-            else if (args[1] == "reactor"){
+            //case 2: reactor
+            else if (args[1].equals("reactor")){
                 Server.reactor(
                     port,
                     Runtime.getRuntime().availableProcessors(),

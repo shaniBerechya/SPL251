54d9d903ea3cc64add7bbf4d0be1ff9af8583a0f
shaniBerechya <berechys@post.bgu.ac.il>
2025-01-21 17:54:57 +0000

    stert of client

--- client/include/StompProtocol.h
+++ client/include/StompProtocol.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "../include/ConnectionHandler.h"
+#include <string> 
+#include <iostream>
 
 // TODO: implement the STOMP protocol
 class StompProtocol
 {
 private:
 public:
+    void processServer();
+    void processKeybord();
 };
